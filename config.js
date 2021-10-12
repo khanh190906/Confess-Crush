a692435cecdee1fe1ea3be15d10070791fb2fbb7
2612thu <92347638+2612thu@users.noreply.github.com>
2021-10-12 09:45:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Dỗi Dương",
-    introTitle: 'Dỗi lắm mà nay sinh nhật nên chiều',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Dỗi lắm mà nay sinh nhật nên phải chiều',
+    introDesc: `Học ăn học nói. Học cả thói bắt nạt người iu`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên <3',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Còn lâu mới được không nhé. Yêu thì nói như đói là phải ăn ^^',
+    question: 'Còn lâu mới chọn không không nhé. Yêu thì nói như đói là phải ăn ^^',
     btnReply: 'Send to Hường <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Iu nhiều nè 😘😘',
     messDesc: 'Cuộc hẹn lúc 6h nè. Anh qua đón thế giới của anh điiiiii',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100017731723838' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100004841112202' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
